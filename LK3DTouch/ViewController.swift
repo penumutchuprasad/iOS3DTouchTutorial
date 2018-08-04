1bd4be7d95e10c188afb05af6c4e8b3167796c58
penumutchu.prasad@gmail.com <penumutchu.prasadgmail.com@penumutchu-macbook-pro.local>
2018-08-04 23:46:12 +0530

    Initial Commit

---
+++ LK3DTouch/ViewController.swift
@@ -0,0 +1,28 @@
+//ViewController.swift
+/*
+ * LK3DTouch
+ * Created by penumutchu.prasad@gmail.com on 04/08/18
+ * Is a product created by abnboys
+ * For the  in the LK3DTouch
+ 
+ * Here the permission is granted to this file with free of use anywhere in the IOS Projects.
+ * Copyright © 2018 ABNBoys.com All rights reserved.
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
